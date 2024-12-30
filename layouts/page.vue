70c2c5d02eade31bf11ea5b65898594efdfb88aa
7iquid <taviefalcon@yahoo.com>
2024-12-30 21:36:02 +0800

    add layout and missing module

---
+++ layouts/page.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>1asdfcaasd  as das dawdawd</h1>
+  <slot />
+  awdaw dawd awd 
+</template>
+
+<script>
+</script>
+
---
+++ pages/[slug].vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+definePageMeta({ layout: 'page' })
+export default {
+    data() {
+        return {
+            userInfo: [
+                { title: 'Username', id: 'username', placeholder: 'Username' },
+                { title: 'Name', id: 'name', placeholder: 'Name' },
+                { title: 'Age', id: 'age', placeholder: 'Age' },
+            ],
+        };
+    },
+};
+</script>
+
+<template >
+    <h1>1asdfcawdaasd asd asd wd</h1>
+    <!-- <form class="login-form">
+        <div v-for="(info, index) in userInfo" :key="index" class="form-field">
+            <span class="form-field__label">{{ info.title }}</span>
+            <InputTextControl :id="info.id" :placeholder="info.placeholder" />
+        </div>
+    </form> -->
+</template>
