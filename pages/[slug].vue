e61c069c314a932955b5e81dccea8d2687f04e30
7iquid <taviefalcon@yahoo.com>
2024-12-30 22:04:21 +0800

    up nuxt layout

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -1,24 +1,24 @@
-<script lang="ts">
-definePageMeta({ layout: 'page' })
-export default {
-    data() {
-        return {
-            userInfo: [
-                { title: 'Username', id: 'username', placeholder: 'Username' },
-                { title: 'Name', id: 'name', placeholder: 'Name' },
-                { title: 'Age', id: 'age', placeholder: 'Age' },
-            ],
-        };
-    },
-};
+<script setup lang="ts">
+interface UserInfo {
+  title: string;
+  id: string;
+  placeholder: string;
+}
+
+const userInfo: UserInfo[] = [
+  { title: 'Username', id: 'username', placeholder: 'Username' },
+  { title: 'Name', id: 'name', placeholder: 'Name' },
+  { title: 'Age', id: 'age', placeholder: 'Age' },
+];
 </script>
 
-<template >
-    <h1>1asdfcawdaasd asd asd wd</h1>
-    <!-- <form class="login-form">
-        <div v-for="(info, index) in userInfo" :key="index" class="form-field">
-            <span class="form-field__label">{{ info.title }}</span>
-            <InputTextControl :id="info.id" :placeholder="info.placeholder" />
-        </div>
-    </form> -->
+<template>
+    <NuxtLayout :name="'page'">
+        <form class="login-form">
+            <div v-for="(info, index) in userInfo" :key="index" class="form-field">
+                <span class="form-field__label">{{ info.title }}</span>
+                <InputTextControl :id="info.id" :placeholder="info.placeholder" />
+            </div>
+        </form>
+    </NuxtLayout>
 </template>
